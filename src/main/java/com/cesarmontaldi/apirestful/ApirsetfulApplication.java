13dfd3a1f858217aa0dd327a7a2d943b5a48b254
Cesar <guto_montaldi@yahoo.com.br>
2023-01-25 10:22:57 -0300

    Project created

---
+++ src/main/java/com/cesarmontaldi/apirestful/ApirsetfulApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmontaldi.apirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirsetfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirsetfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmontaldi/apirestful/ApirsetfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmontaldi.apirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirsetfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
