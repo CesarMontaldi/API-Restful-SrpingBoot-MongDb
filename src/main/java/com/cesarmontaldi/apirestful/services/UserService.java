14756467d7603c2078a43dfeddf41439ea29b1e7
Cesar <guto_montaldi@yahoo.com.br>
2023-01-26 11:22:36 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/cesarmontaldi/apirestful/services/UserService.java
+++ src/main/java/com/cesarmontaldi/apirestful/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.cesarmontaldi.apirestful.domain.User;
@@ -16,20 +17,26 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
+
 	
 	public List<User> findAll() {
 		return repo.findAll(); 
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id); 
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Obejeto não encontrado.")); 
 	} 
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj); 
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail()); 
 	}
