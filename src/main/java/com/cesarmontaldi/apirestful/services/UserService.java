dd8c1a623e5784b8556ce5d28c0293bb6096a607
Cesar <guto_montaldi@yahoo.com.br>
2023-01-26 12:34:58 -0300

    Atualização de usuário com PUT

--- src/main/java/com/cesarmontaldi/apirestful/services/UserService.java
+++ src/main/java/com/cesarmontaldi/apirestful/services/UserService.java
@@ -23,7 +23,7 @@ public class UserService {
 		return repo.findAll(); 
 	}
 
-	public User findById(String id) {
+	public User findById(String id) { 
 		Optional<User> obj = repo.findById(id); 
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Obejeto não encontrado.")); 
 	} 
@@ -35,6 +35,17 @@ public class UserService {
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId()); 
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 
 	public User fromDTO(UserDTO objDto) {
