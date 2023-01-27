9f3da2f97ca25d0c113afa0c4d0174765371b358
Cesar <guto_montaldi@yahoo.com.br>
2023-01-27 15:56:15 -0300

    Obtendo um post por id

---
+++ src/main/java/com/cesarmontaldi/apirestful/services/PostService.java
@@ -0,0 +1,24 @@
+package com.cesarmontaldi.apirestful.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cesarmontaldi.apirestful.domain.Post;
+import com.cesarmontaldi.apirestful.repository.PostRepository;
+import com.cesarmontaldi.apirestful.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) { 
+		Optional<Post> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Obejeto não encontrado.")); 
+	} 
+
+}
+ 
